b72c43d708f2386154d902df86619339a9c4f14d
Abdulsalam-Baruwa <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2022-04-11 05:29:08 +0100

    attempt; succeeded, failed

---
+++ week3/retry_func.py
@@ -0,0 +1,10 @@
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break
+    else:
+      print("Attempt " + str(n) + " failed")
+
+print(retry(create_user, 3))
+print(retry(stop_service, 5))
